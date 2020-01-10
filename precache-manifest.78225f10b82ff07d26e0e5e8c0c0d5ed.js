83b61862e18c35aa5bd9226b3807de75d2dad8a7
Luis Waderdorff <walderdorff@live.de>
2020-01-10 16:41:43 +0100

    Updates

---
+++ precache-manifest.78225f10b82ff07d26e0e5e8c0c0d5ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06422f8a4ea69066224427adbaf9f243",
+    "url": "/wetterapplikation/index.html"
+  },
+  {
+    "revision": "020863ca9bd9a7e477b0",
+    "url": "/wetterapplikation/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8ca39362b25da437e1c2",
+    "url": "/wetterapplikation/static/css/main.f244a254.chunk.css"
+  },
+  {
+    "revision": "020863ca9bd9a7e477b0",
+    "url": "/wetterapplikation/static/js/2.80e877f8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/wetterapplikation/static/js/2.80e877f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ca39362b25da437e1c2",
+    "url": "/wetterapplikation/static/js/main.14e3adb7.chunk.js"
+  },
+  {
+    "revision": "d94faa511e6e74b366cf",
+    "url": "/wetterapplikation/static/js/runtime-main.aece33d5.js"
+  }
+]);
